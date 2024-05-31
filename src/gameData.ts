6c292e180a7b3225fcf8953eddd869e019533f24
Dreukrag <filipegroh@gmail.com>
2024-05-31 10:47:13 -0300

    - House keeping, gamedata removed from data and sent to its own folder

--- src/gameData.ts
+++
@@ -1,7 +0,0 @@
-import { RealityBubble } from "./types/database/Reality";
-import { UserSession } from "./types/database/User";
-import { Character } from "./types/live/Character";
-
-const uraferGameStateRealityBubbles: RealityBubble[] = [];
-
-const onlineCharactersMap: { [key: string]: Character } = {};
